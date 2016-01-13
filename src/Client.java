74dc7715a5face63a6f2e953c1c09dcbcb269286
Arif Rahman Ibrahim <ongkaqrif@gmail.com>
2016-01-13 19:07:06 +0700

    This commit works. If I keep messing up, then I'd just revert to this.

--- src/Client.java
+++ src/Client.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Client {
 	public static void main(String[] args) throws IOException{
-		String rps;
+		String rps = " ";
+		boolean flagclient,flagserver;
 		Scanner scan = new Scanner(System.in);
 		if(args.length != 3){
 			throw new IllegalArgumentException("Usage: client <Server> <Port>");
@@ -17,30 +18,40 @@ public class Client {
 		
 		System.out.println("Connected to " + args[1] + " on port " + args[2]);
 		
-		do{
-			System.out.println("Rock/Paper/Scissors: ");
-			rps = scan.nextLine();
-		}while(rps.equalsIgnoreCase("Rock") == false && rps.equalsIgnoreCase("Paper") == false && rps.equalsIgnoreCase("Scissors") == false);
-		rps = Character.toUpperCase(rps.charAt(0)) + rps.substring(1);
-		System.out.println("You chose: " + rps);
-		
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		out.writeByte(1);
-		out.writeUTF(rps);
-		out.flush();
-		
-		
-		if(in.readByte() == 3){
-			System.out.println("Opponent Chose: " + in.readUTF());
-		}
-		
-		if(in.readByte() == 2){
-			System.out.println(in.readUTF());
-		}
-		
-		socket.close();
-		
+			do{
+				out.writeByte(1);
+				out.writeUTF(" ");
+				System.out.println("Rock/Paper/Scissors (Type Exit to quit): ");
+				rps = scan.nextLine();
+			}while(rps.equalsIgnoreCase("Rock") == false && rps.equalsIgnoreCase("Paper") == false && rps.equalsIgnoreCase("Scissors") == false && rps.equalsIgnoreCase("Exit") == false);
+			if(rps.equals("Exit")){
+				socket.close();
+				System.out.println("\n\n\nGame Over");
+				scan.nextLine();
+				return;
+			}
+			rps = Character.toUpperCase(rps.charAt(0)) + rps.toLowerCase().substring(1);
+			System.out.println("You chose: " + rps);
+			System.out.println("Waiting for host..");
+			out.writeByte(1);
+			out.writeUTF(rps);
+			out.flush();
+			
+			do{
+				
+			}while(in.readByte() == 2 && in.readUTF() == " ");
+			
+			if(in.readByte() == 3){
+				System.out.println("Opponent Chose: " + in.readUTF());
+			}
+			
+			if(in.readByte() == 2){
+				System.out.println(in.readUTF());
+			}
+			
+			socket.close();
 		scan.nextLine();
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import java.io.*;
 
@@ -8,7 +9,10 @@ public class Server {
 		if(args.length != 2){
 			throw new IllegalArgumentException("Usage: server <port>");
 		}
-		String random[] = {"Rock", "Paper", "Scissors"};
+		String opponent = " ";
+		//String random[] = {"Rock", "Paper", "Scissors"};
+		boolean flagclient, flagserver;
+		Scanner scan = new Scanner(System.in);
 		String result;
 		String rps = null;
 	
@@ -16,68 +20,74 @@ public class Server {
 		
 		ServerSocket servSock = new ServerSocket(servPort);
 		
-		for(;;){
-			Socket clntSock = servSock.accept();
-			System.out.println("Client " + clntSock.getInetAddress().getHostAddress() + " connected on port" + clntSock.getPort());
-			DataInputStream in = new DataInputStream(clntSock.getInputStream());
-			DataOutputStream out = new DataOutputStream(clntSock.getOutputStream());
+		Socket clntSock = servSock.accept();
+		System.out.println("Client " + clntSock.getInetAddress().getHostAddress() + " connected on port" + clntSock.getPort());
+		DataInputStream in = new DataInputStream(clntSock.getInputStream());
+		DataOutputStream out = new DataOutputStream(clntSock.getOutputStream());
+		do{
+			out.writeByte(2);
+			out.writeUTF(" ");
+			System.out.println("Rock/Paper/Scissors (Type Exit to quit): ");
+			opponent = scan.nextLine();
+		}while(opponent.equalsIgnoreCase("Rock") == false && opponent.equalsIgnoreCase("Paper") == false && opponent.equalsIgnoreCase("Scissors") == false);
+		
+		opponent = Character.toUpperCase(opponent.charAt(0)) + opponent.toLowerCase().substring(1);
+		
+		System.out.println("You Chose: " + opponent);
+		System.out.println("Waiting for client..");
+		do{
+			//do nothing until client sends a data
+		}while(in.readByte() == 1 && in.readUTF() == " ");
+		
+		while(true){
+			byte MessageType = in.readByte();
 			
-			while(true){
-				byte MessageType = in.readByte();
-				
-				switch(MessageType){
-				case 1:
-					rps = in.readUTF();
-					System.out.println("Client chose: " + rps);
-				}
+			if(MessageType == 1)
+				rps = in.readUTF();
+				System.out.println("Opponent chose: " + rps);
 				break;
-			}
-			
-			String opponent = random[ThreadLocalRandom.current().nextInt(0, 3)];
-			System.out.println("Server Chose: " + opponent);
-			out.writeByte(3);
-			out.writeUTF(opponent);
-			
-			if(rps.equalsIgnoreCase("Rock") == true && opponent.equalsIgnoreCase("Scissors") == true){
-				out.writeByte(2);
-				out.writeUTF("You win");
-				out.flush();
-			}
-			else if(rps.equalsIgnoreCase("Rock") == true && opponent.equalsIgnoreCase("Paper") == true){
-				out.writeByte(2);
-				out.writeUTF("You lose");
-				out.flush();
-			}
-			else if(rps.equalsIgnoreCase("Paper") == true && opponent.equalsIgnoreCase("Rock") == true){
-				out.writeByte(2);
-				out.writeUTF("You win");
-				out.flush();
-			}
-			else if(rps.equals("Paper") == true && opponent.equals("Scissors") == true){
-				out.writeByte(2);
-				out.writeUTF("You lose");
-				out.flush();
-			}
-			else if(rps.equals("Scissors") == true && opponent.equals("Paper") == true){
-				out.writeByte(2);
-				out.writeUTF("You lose");
-				out.flush();
-			}
-			else if(rps.equals("Scissors") == true && opponent.equals("Rock") == true){
-				out.writeByte(2);
-				out.writeUTF("You lose");
-				out.flush();
-			}
-			else if(rps.equals(opponent)){
-				out.writeByte(2);
-				out.writeUTF("It's a draw");
-				out.flush();
-			}
-			
-			
-			clntSock.close();
 		}
-		
-		
-	}
+			
+		out.writeByte(3);
+		out.writeUTF(opponent);
+			
+		if(rps.equalsIgnoreCase("Rock") == true && opponent.equalsIgnoreCase("Scissors") == true){
+			out.writeByte(2);
+			out.writeUTF("You win");System.out.println("You Lose");
+			out.flush();
+		}
+		else if(rps.equalsIgnoreCase("Rock") == true && opponent.equalsIgnoreCase("Paper") == true){
+			out.writeByte(2);
+			out.writeUTF("You lose");System.out.println("You Win");
+			out.flush();
+		}
+		else if(rps.equalsIgnoreCase("Paper") == true && opponent.equalsIgnoreCase("Rock") == true){
+			out.writeByte(2);
+			out.writeUTF("You win");System.out.println("You Lose");
+			out.flush();
+		}
+		else if(rps.equals("Paper") == true && opponent.equals("Scissors") == true){
+			out.writeByte(2);
+			out.writeUTF("You lose");System.out.println("You Win");
+			out.flush();
+		}
+		else if(rps.equals("Scissors") == true && opponent.equals("Paper") == true){
+			out.writeByte(2);
+			out.writeUTF("You lose");System.out.println("You Win");
+			out.flush();
+		}
+		else if(rps.equals("Scissors") == true && opponent.equals("Rock") == true){
+			out.writeByte(2);
+			out.writeUTF("You lose");System.out.println("You Win");
+			out.flush();
+		}
+		else if(rps.equals(opponent)){
+			out.writeByte(2);
+			out.writeUTF("It's a draw");System.out.println("It's a draw");
+			out.flush();
+		}
+			
+		clntSock.close();
+		scan.nextLine();
+		}
 }
